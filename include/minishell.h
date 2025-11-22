75dbecc16ad390daa6e472cc7da1341ab8f74e57
biraj-phuyal <birajphuyal11@gmail.com>
2025-11-22 13:50:33 +0000

    Start of the project

---
+++ include/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 13:47:34 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/11/22 13:48:41 by biphuyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
