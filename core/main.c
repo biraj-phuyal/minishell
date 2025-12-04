820bfbf2122e7011492237e2d14ab4e26356ac73
Guilherme De Andrade Sales <gude-and@c2r3s11.42lisboa.com>
2025-12-04 15:50:09 +0000

    Fixing the conflicts problem

--- core/main.c
+++ core/main.c
@@ -6,13 +6,14 @@
 /*   By: gude-and <gude-and@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 13:48:46 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/11/22 19:54:32 by gude-and         ###   ########.fr       */
+/*   Updated: 2025/12/04 15:49:38 by gude-and         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int args, char **argv, char **env)
+int	main(int args, char **argv, char **envp)
 {
-	
+    ((void)args, (void)argv);
+	init_env(envp);
 }
