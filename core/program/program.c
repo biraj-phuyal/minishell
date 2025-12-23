4319948c711946596d9ed0480e9f76b9ba2d1346
biraj-phuyal <birajphuyal11@gmail.com>
2025-12-23 16:07:04 +0000

    base of main-lopp is done

--- core/program/program.c
+++ core/program/program.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 16:23:33 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/12/18 14:19:23 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/12/23 16:05:25 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,26 @@
 
 void	program_loop(t_env *env)
 {
-	const char	*input;
-	t_command	*command;
+	char	*input;
+	t_ast_node	*ast;
+	char		**envp;
+	int			last_status;
 
+	last_status = 0;
 	while (1)
 	{
 		input = readline("minishell: ");
 		if (!input)
 			free_list_and_exit(env);
-		command = command_line(lexer(input));
-		execute();
+		if (input)
+			add_history(input);
+		envp = list_to_array(env);
+		ast = parse(input, last_status, envp);
+		free(input);
+		if (!ast)
+			continue ;
+		last_status = execute(ast, &env, envp);
+		ast_free(ast);
+		free_envp(envp);
 	}
 }
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,28 +6,30 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 13:47:34 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/12/18 12:26:01 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/12/23 15:40:10 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <strings.h>
-# include <stdlib.h>
 # include <stdio.h>
-# include <unistd.h>
-# include <stdbool.h>
 # include <fcntl.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <signal.h>
-# include <stdint.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <execution.h>
 # include <lexer.h>
 # include <utils.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <parser.h>
+# include <strings.h>
+# include <stdbool.h>
+# include <sys/wait.h>
+# include <expander.h>
+# include <sys/types.h>
+# include <execution.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 void	program_loop(t_env *env);
 
