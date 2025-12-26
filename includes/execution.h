a96d40f351f8bd7f158bb6f85b77e9425823e90f
biraj-phuyal <birajphuyal11@gmail.com>
2025-12-26 17:49:43 +0000

    Replacement of funtions and starting on export

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 19:32:47 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/12/26 16:58:09 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/12/26 17:45:19 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void		order_env(char **env);
 void		print_pwd(t_env *env);
 t_env		*init_env(char **envp);
 void		free_envp(char **envp);
+char		*get_new_dir(t_cmd *cmd);
 void		exit_program(t_env *env);
 void		print_all_env(t_env *env);
 char		*get_key(const char *envp);
@@ -43,11 +44,13 @@ char		**list_to_array(const t_env *env);
 void		new_pwd(t_env **env, char *old_pwd);
 void		old_pwd(t_env **env, char *old_pwd);
 void		push_back(t_env **head, t_env *new);
+void		export(t_env *env, const char *args);
 char		*return_value(t_env *env, char *key);
 int			full_length_of_list(const t_env *env);
 void		change_dir(t_env **env, char *new_dir);
 void		update_old_pwd(t_env *env, char *og_pwd);
 void		create_old_pwd(t_env *env, char *og_pwd);
+void		execute_cd(t_cmd *cmd, int i, t_env **env);
 void		destroy_and_copy(char **dest, const char *src);
 int			execute_builtin(t_cmd *cmd, int i, t_env **env);
 int			execute(t_ast_node *ast, t_env **env, char **envp);
