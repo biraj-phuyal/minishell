f43e927d3d9cea726d56d1c65a6587cc438ffd06
biraj-phuyal <birajphuyal11@gmail.com>
2025-12-26 20:15:27 +0000

    exit is working

--- core/execution/execute/execute_builtin.c
+++ core/execution/execute/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 16:00:47 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/12/26 19:59:34 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/12/26 20:14:36 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@ int	execute_builtin(t_cmd *cmd, int i, t_env **env)
 		*env = unset(*env, cmd->argv[i + 1]);
 	else if (ft_strcmp(cmd->argv[i], "env") == 0)
 		print_all_env(*env);
+	else if (ft_strcmp(cmd->argv[i], "exit") == 0)
+		return (1);
 	return (0);
 }
--- core/program/program.c
+++ core/program/program.c
@@ -6,18 +6,45 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 16:23:33 by biphuyal          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/01/03 18:08:19 by biphuyal         ###   ########.fr       */
+=======
+/*   Updated: 2025/12/26 20:13:31 by biphuyal         ###   ########.fr       */
+>>>>>>> e5eb1cc (exit is working)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	program_loop(t_env *env)
+static int	process_input(char *input, int last_status, t_env **env)
 {
-	char		*input;
 	t_ast_node	*ast;
 	char		**envp;
-	int			last_status;
+	int			status;
+
+	envp = list_to_array(*env);
+	ast = parse(input, last_status, envp);
+	if (!ast)
+	{
+		free_envp(envp);
+		return (last_status);
+	}
+	status = execute(ast, env, envp);
+	if (status == 1)
+	{
+		ast_free(ast);
+		free_envp(envp);
+		exit_program(*env);
+	}
+	ast_free(ast);
+	free_envp(envp);
+	return (status);
+}
+
+void	program_loop(t_env *env)
+{
+	char	*input;
+	int		last_status;
 
 	last_status = 0;
 	while (1)
@@ -27,15 +54,7 @@ void	program_loop(t_env *env)
 			free_list_and_exit(env);
 		if (input)
 			add_history(input);
-		envp = list_to_array(env);
-		ast = parse(input, last_status, envp);
+		last_status = process_input(input, last_status, &env);
 		free(input);
-		if (!ast)
-			continue ;
-		last_status = execute(ast, &env, envp);
-		ast_free(ast);
-		free_envp(envp);
-		if (last_status == 1)
-			exit_program(env);
 	}
 }
