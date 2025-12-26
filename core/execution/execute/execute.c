124f3bb5c70faa9eff5548e68b79a84f2ce1b775
biraj-phuyal <birajphuyal11@gmail.com>
2025-12-26 17:41:02 +0000

    cd and pwd is working

--- core/execution/execute/execute.c
+++ core/execution/execute/execute.c
@@ -6,11 +6,18 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 14:09:11 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/12/26 16:02:31 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/12/26 17:00:57 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+int	execute_one_command(t_cmd *cmd, t_env **env)
+{
+	if (is_builtin(cmd, 0))
+		return (execute_builtin(cmd, 0, env));
+	return (0);
+}
 
 int	execute(t_ast_node *ast, t_env **env, char **envp)
 {
@@ -18,11 +25,6 @@ int	execute(t_ast_node *ast, t_env **env, char **envp)
 	(void)envp;
 
 	if (ast->type == NODE_CMD)
-		execute_one_command(ast->cmd, env, envp);
-	
-}
-void	execute_one_command(t_cmd *cmd, t_env **env, char **envp)
-{
-	if (is_builtin(cmd, 0))
-		execute_builtin(cmd);
+		return (execute_one_command(ast->cmd, env));
+	return (0);
 }
