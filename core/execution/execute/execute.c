507893496d812153c4bdbcf0fc373f0ccf994a23
biraj-phuyal <birajphuyal11@gmail.com>
2025-12-27 00:19:55 +0000

    Learning about pipe, fork, dup2, waitpid, etc...

--- core/execution/execute/execute.c
+++ core/execution/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 14:09:11 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/12/26 17:00:57 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/12/26 23:36:24 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,25 @@ int	execute_one_command(t_cmd *cmd, t_env **env)
 
 int	execute(t_ast_node *ast, t_env **env, char **envp)
 {
-	(void)env;
-	(void)envp;
+	int	fds[2];
+	int	i;
+	int	pid;
 
 	if (ast->type == NODE_CMD)
 		return (execute_one_command(ast->cmd, env));
+	i = 0;
+	while (i < ast->cmd->argc)
+	{
+		if (ast->right != NULL)
+		{
+			pipe(fds);
+			pid = fork();
+			if (pid == 0)
+			{
+				
+			}
+		}
+	}
+	
 	return (0);
 }
