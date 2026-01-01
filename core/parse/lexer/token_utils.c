3f87fc02f0de6e7981db006edb5a2736e2403aee
Guiandrade10 <guiandrade622@gmail.com>
2026-01-01 15:06:32 +0000

    Report syntax errors for unclosed quotes in lexer

--- core/parse/lexer/token_utils.c
+++ core/parse/lexer/token_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: gude-and <gude-and@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 14:55:47 by gude-and          #+#    #+#             */
-/*   Updated: 2025/12/23 15:14:31 by biphuyal         ###   ########.fr       */
+/*   Updated: 2026/01/01 15:05:54 by gude-and         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,13 @@ bool	advance_to_word_end(t_lexer *lex)
 		{
 			new_pos = skip_quoted_section(lex->input, lex->pos);
 			if (new_pos == -1)
+			{
+				ft_putstr_fd("minishell: unexpected EOF while looking for matching `", 2);
+				ft_putchar_fd(c, 2);
+				ft_putstr_fd("'", 2);
+				ft_putchar_fd('\n', 2);
 				return (false);
+			}
 			lex->pos = new_pos;
 		}
 		else
